19ef0d85a138277784000ba818766c118cc8f907
Tomáš Doležal <tomas.dolezal@vzp.cz>
2015-09-29 14:49:49 +0200

    Project filtering optimization, file processing heuristcs

--- T4TS/Traversal/ProjectTraverser.cs
+++ T4TS/Traversal/ProjectTraverser.cs
@@ -1,11 +1,14 @@
 ﻿using EnvDTE;
 using System;
+using System.IO;
 using System.Linq;
 
 namespace T4TS
 {
     public class ProjectTraverser
     {
+        static readonly Guid SolutionItemKind = Guid.Parse(Constants.vsProjectKindSolutionItems);
+
         public Action<CodeNamespace> WithNamespace { get; private set; }
 
         public ProjectTraverser(Project project, Action<CodeNamespace> withNamespace)
@@ -18,21 +21,42 @@ namespace T4TS
 
             WithNamespace = withNamespace;
 
+            TraverseProject(project);
+        }
+
+        private void TraverseProject(Project project)
+        {
             if (project.ProjectItems != null)
-                Traverse(project.ProjectItems);
+            {
+                // traverse projects inside solution items
+                if (Guid.Parse(project.Kind) == SolutionItemKind)
+                {
+                    foreach (ProjectItem pi in project.ProjectItems)
+                    {
+                        if (pi.SubProject != null)
+                            TraverseProject(pi.SubProject);
+                    }
+                }
+
+                // normal projects
+                else if (CodeTraverser.Settings.ProjectNamesToProcess == null ||
+                         CodeTraverser.Settings.ProjectNamesToProcess.Contains(project.Name))
+                {
+                    Traverse(project.ProjectItems);
+                }
+            }
         }
 
         private void Traverse(ProjectItems items)
         {
             foreach (ProjectItem pi in items)
             {
-                if (pi.FileCodeModel != null)
+                if (ShouldProcessItem(pi))
                 {
-                    if (CodeTraverser.Settings.ProjectNamesToProcess == null ||
-                        CodeTraverser.Settings.ProjectNamesToProcess.Contains(pi.ContainingProject.Name))
+                    if (pi.FileCodeModel != null)
                     {
                         var codeElements = pi.FileCodeModel.CodeElements;
-                        
+
                         foreach (object elem in codeElements)
                         {
                             if (elem is CodeNamespace)
@@ -43,9 +67,21 @@ namespace T4TS
 
                 if (pi.ProjectItems != null)
                     Traverse(pi.ProjectItems);
-                else if (pi.SubProject != null && pi.SubProject.ProjectItems != null)
-                    Traverse(pi.SubProject.ProjectItems);
             }
+        }
+
+        private bool ShouldProcessItem(ProjectItem pi)
+        {
+            var fileName = pi.FileNames[0];
+
+            if (System.IO.Path.GetExtension(fileName) == ".cs")
+            {
+                var content = System.IO.File.ReadAllText(fileName);
+                if (content.Contains("TypeScript"))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
